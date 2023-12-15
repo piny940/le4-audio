96f1c5622edf4661c9bec5ce31887e0fe199eeb5
piny940 <an.s.shoma@gmail.com>
2023-12-15 16:34:40 +0900

    fix label

--- gui/view/figures.py
+++ gui/view/figures.py
@@ -16,7 +16,7 @@ class Figures:
 
   def draw(self, spectrogram, f0s):
     for ax in self.__fig.axes:
-      ax.cla()
+      self.__fig.delaxes(ax)
     self.__spec_ax = self.__fig.add_subplot(111)
     SpecWithF0s(self.__spec_ax).draw(spectrogram, f0s)
     self.__fig.tight_layout()
@@ -26,7 +26,7 @@ class Figures:
 class SpecWithF0s:
   def __init__(self, ax: Axes):
     self.__ax = ax
-    self.__ax.set_xlabel('sec')
+    self.__ax.set_xlabel('sample')
     self.__ax.set_ylabel('frequency [Hz]')
 
   def draw(self, spectrogram, f0s):
@@ -34,10 +34,11 @@ class SpecWithF0s:
         np.flipud(np.array(spectrogram).T),
         extent=[0, len(spectrogram), 0, SR / 2],
         aspect='auto',
-        interpolation='nearest'
+        interpolation='nearest',
     )
     x_data = np.linspace(0, len(spectrogram), len(f0s))
     self.__ax.plot(x_data, f0s)
+    self.__ax.set_ylim(0, 3000)
 
 
 class Spectrogram:
