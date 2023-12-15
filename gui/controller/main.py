bbb207bbf4a96dc379a78d811dbcad9da7de8e75
piny940 <an.s.shoma@gmail.com>
2023-12-15 16:59:11 +0900

    WaveRangeを使用

--- gui/controller/main.py
+++ gui/controller/main.py
@@ -3,6 +3,7 @@ from .interface import IController
 from core.core import *
 from core.constants import SR, SIZE_FRAME, SHIFT_SIZE
 import matplotlib.pyplot as plt
+from core.wave_range import WaveRange
 
 
 class Controller(IController):
@@ -12,9 +13,14 @@ class Controller(IController):
   def main(self):
     plt.rcParams.update({'font.size': 6})
     self.__view.create_window()
+    self.__wave_range = None
 
   def load_file(self, filename):
     waveform = load_waveform(filename)
+    self.__wave_range = WaveRange(waveform)
+    self.update_figures(self.__wave_range.get_waveform())
+
+  def update_figures(self, waveform):
     spec = spectrogram(waveform, SIZE_FRAME, SHIFT_SIZE)
     f0s = get_f0s(waveform, SR, SIZE_FRAME)
     self.__view.figures.draw(spec, f0s)
