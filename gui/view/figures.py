1ef659e7016e64c412ee77ee46194d2d196bf0c1
piny940 <an.s.shoma@gmail.com>
2023-12-21 16:37:31 +0900

    Melodyを作成

--- gui/view/figures.py
+++ gui/view/figures.py
@@ -11,14 +11,17 @@ class Figures:
     self.__frame = frame
     self.__fig = plt.figure(figsize=(3, 2))
     self.__spec_ax = None
+    self.__melody_ax = None
     canvas = FigureCanvasTkAgg(self.__fig, master=self.__frame)
     canvas.get_tk_widget().pack(side='left')
 
-  def draw(self, spectrogram, f0s, wave_range: WaveRange):
+  def draw(self, spectrogram, f0s, melody, wave_range: WaveRange):
     for ax in self.__fig.axes:
       self.__fig.delaxes(ax)
-    self.__spec_ax = self.__fig.add_subplot(111)
+    self.__spec_ax = self.__fig.add_subplot(121)
     SpecWithF0s(self.__spec_ax).draw(spectrogram, f0s, wave_range)
+    self.__melody_ax = self.__fig.add_subplot(122)
+    Melody(self.__melody_ax).draw(melody)
     self.__fig.tight_layout()
     self.__fig.canvas.draw()
 
@@ -41,27 +44,37 @@ class SpecWithF0s:
     self.__ax.set_xlim(wave_range.get_start() // SHIFT_SIZE, min(wave_range.get_end() // SHIFT_SIZE, len(spectrogram)))
     self.__ax.set_ylim(0, 3000)
 
-
-class Spectrogram:
+class Melody:
   def __init__(self, ax: Axes):
     self.__ax = ax
-    self.__ax.set_xlabel('sec')
-    self.__ax.set_ylabel('frequency [Hz]')
+    self.__ax.set_xlabel('sample')
+    self.__ax.set_ylabel('melody')
 
-  def draw(self, spectrogram):
-    self.__ax.imshow(
-        np.flipud(np.array(spectrogram).T),
-        extent=[0, len(spectrogram), 0, SR / 2],
-        aspect='auto',
-        interpolation='nearest'
-    )
+  def draw(self, melody, wave_range: WaveRange):
+    self.__ax.plot(melody)
+    self.__ax.set_xlim(wave_range.get_start() // SHIFT_SIZE, min(wave_range.get_end() // SHIFT_SIZE, len(melody)))
 
 
-class F0s:
-  def __init__(self, ax: Axes):
-    self.__ax = ax
-    self.__ax.set_xlabel('sec')
-    self.__ax.set_ylabel('frequency [Hz]')
+# class Spectrogram:
+#   def __init__(self, ax: Axes):
+#     self.__ax = ax
+#     self.__ax.set_xlabel('sec')
+#     self.__ax.set_ylabel('frequency [Hz]')
 
-  def draw(self, f0s):
-    self.__ax.plot(f0s)
+#   def draw(self, spectrogram):
+#     self.__ax.imshow(
+#         np.flipud(np.array(spectrogram).T),
+#         extent=[0, len(spectrogram), 0, SR / 2],
+#         aspect='auto',
+#         interpolation='nearest'
+#     )
+
+
+# class F0s:
+#   def __init__(self, ax: Axes):
+#     self.__ax = ax
+#     self.__ax.set_xlabel('sec')
+#     self.__ax.set_ylabel('frequency [Hz]')
+
+#   def draw(self, f0s):
+#     self.__ax.plot(f0s)
