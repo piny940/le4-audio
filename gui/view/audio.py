68e6660e0208e5e521f83b1182f90c54633977c1
piny940 <an.s.shoma@gmail.com>
2023-12-22 13:29:55 +0900

    AudioPlayerを作成

--- gui/view/audio.py
+++ gui/view/audio.py
@@ -1,16 +1,26 @@
-import simpleaudio
+import simpleaudio as sa
+from core.constants import *
 
 class AudioPlayer:
   def __init__(self):
-    self.__playing = False
     self.__waveform = None
+    self.__wave_obj = None
+    self.__play_obj = None
   
   def set_wave(self, waveform):
     self.__waveform = waveform
+    self.__wave_obj = sa.WaveObject(waveform, 1, 2, SR)
   
   def play(self):
     if self.__waveform is None:
       print("No waveform to play")
       return
-    self.__playing = True
+    self.__play_obj = self.__wave_obj.play()
     
+  def stop(self):
+    self.__play_obj.wait_done()
+
+  def is_playing(self) -> bool:
+    if self.__play_obj is None:
+      return False
+    return self.__play_obj.is_playing()
