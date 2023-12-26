4c1c46551f973a4dfa458acb82a667741abcf3eb
piny940 <an.s.shoma@gmail.com>
2023-12-26 09:21:51 +0900

    vibrato

--- gui/view/control_panel.py
+++ gui/view/control_panel.py
@@ -2,6 +2,7 @@ import tkinter as tk
 from .voice_change_panel import VoiceChangePanel
 from .view_base import ViewBase
 from .tremolo_panel import TremoloPanel
+from .vibrato_panel import VibratoPanel
 
 class ControlPanel(ViewBase):
   def draw(self):
@@ -25,8 +26,20 @@ class ControlPanel(ViewBase):
       highlightthickness=1
     )
     self.tremolo = TremoloPanel(tremolo_frame, self._c)
+    vibrato_frame = tk.Frame(
+      self._frame,
+      width=400,
+      height=100,
+      padx=10,
+      pady=10,
+      highlightbackground="white",
+      highlightthickness=1
+    )
+    self.vibrato = VibratoPanel(vibrato_frame, self._c)
     vc_frame.pack(side=tk.LEFT)
     tremolo_frame.pack(side=tk.LEFT)
+    vibrato_frame.pack(side=tk.LEFT)
     self.voice_change.draw()
     self.tremolo.draw()
+    self.vibrato.draw()
     self._set([vc_frame, tremolo_frame])
---
+++ gui/view/vibrato_panel.py
@@ -0,0 +1,29 @@
+from .view_base import ViewBase
+import tkinter as tk
+from .text_box import TextBox
+
+class VibratoPanel(ViewBase):
+  def draw(self):
+    title = tk.Label(
+      master=self._frame,
+      text="Vibrato",
+      font=("Helvetica", 20)
+    )
+    text_boxes_frame = tk.Frame(
+      self._frame,
+      width=400,
+      height=100,
+      padx=10,
+      pady=10
+    )
+    self.frequency_box = TextBox(text_boxes_frame, self._c)
+    apply_button = tk.Button(
+      master=self._frame,
+      text="Apply",
+      # command=self._c.apply_voice_change
+    )
+    title.pack(side=tk.TOP)
+    text_boxes_frame.pack(side=tk.TOP)
+    apply_button.pack(side=tk.TOP)
+    self.frequency_box.draw("Frequency", 10)
+    self._set([title, self.frequency_box, apply_button])
